138ec6c37daf66ad33e33f9e37da8f5902c19302
성수천 <3598ysm@gmail.com>
2023-07-27 14:44:21 +0900

    Subjcet

--- src/main/java/com/green/secondproject/config/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/green/secondproject/config/security/CustomAuthenticationEntryPoint.java
@@ -1,31 +1,31 @@
-package com.green.secondproject.config.security;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.green.security.config.security.model.EntryPointErrorResponse;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-
-import java.io.IOException;
-
-@Slf4j
-public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
-
-    @Override
-    public void commence(HttpServletRequest req, HttpServletResponse res, AuthenticationException authException) throws IOException, ServletException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        log.info("[commence] 인증 실패로 response.sendError 발생");
-
-        EntryPointErrorResponse msg = new EntryPointErrorResponse();
-        msg.setMsg("인증이 실패하였습니다.");
-
-        res.setStatus(401);
-        res.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        res.setCharacterEncoding("UTF-8");
-        res.getWriter().write(objectMapper.writeValueAsString(msg));
-    }
-}
+//package com.green.secondproject.config.security;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.green.security.config.security.model.EntryPointErrorResponse;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.MediaType;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.web.AuthenticationEntryPoint;
+//
+//import java.io.IOException;
+//
+//@Slf4j
+//public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+//
+//    @Override
+//    public void commence(HttpServletRequest req, HttpServletResponse res, AuthenticationException authException) throws IOException, ServletException {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        log.info("[commence] 인증 실패로 response.sendError 발생");
+//
+//        EntryPointErrorResponse msg = new EntryPointErrorResponse();
+//        msg.setMsg("인증이 실패하였습니다.");
+//
+//        res.setStatus(401);
+//        res.setContentType(MediaType.APPLICATION_JSON_VALUE);
+//        res.setCharacterEncoding("UTF-8");
+//        res.getWriter().write(objectMapper.writeValueAsString(msg));
+//    }
+//}
--- src/main/java/com/green/secondproject/config/security/model/MyUserDetails.java
+++ src/main/java/com/green/secondproject/config/security/model/MyUserDetails.java
@@ -1,48 +1,48 @@
-package com.green.secondproject.config.security.model;
-
-import lombok.Builder;
-import lombok.Data;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Data
-@Builder
-public class MyUserDetails implements UserDetails {
-    private Long iuser;
-    private String uid;
-    private String upw;
-    private String name;
-
-    @Builder.Default
-    private List<String> roles = new ArrayList<>();
-
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
-    }
-
-    @Override
-    public String getPassword() { return this.upw; }
-
-    @Override
-    public String getUsername() { return this.uid; }
-
-    @Override
-    public boolean isAccountNonExpired() { return true; }
-
-    @Override
-    public boolean isAccountNonLocked() { return true; }
-
-    @Override
-    public boolean isCredentialsNonExpired() { return true; }
-
-    @Override
-    public boolean isEnabled() { return true; }
-}
+//package com.green.secondproject.config.security.model;
+//
+//import lombok.Builder;
+//import lombok.Data;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.ArrayList;
+//import java.util.Collection;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//@Data
+//@Builder
+//public class MyUserDetails implements UserDetails {
+//    private Long iuser;
+//    private String uid;
+//    private String upw;
+//    private String name;
+//
+//    @Builder.Default
+//    private List<String> roles = new ArrayList<>();
+//
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+//    }
+//
+//    @Override
+//    public String getPassword() { return this.upw; }
+//
+//    @Override
+//    public String getUsername() { return this.uid; }
+//
+//    @Override
+//    public boolean isAccountNonExpired() { return true; }
+//
+//    @Override
+//    public boolean isAccountNonLocked() { return true; }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() { return true; }
+//
+//    @Override
+//    public boolean isEnabled() { return true; }
+//}
