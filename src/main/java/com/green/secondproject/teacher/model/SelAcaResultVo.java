f29f13dde96218d0481d6be022e951c57e1d9220
노정민 <ssolen@naver.com>
2023-07-29 14:49:32 +0900

    Operation 작업 및 성적 select 임시작업

---
+++ src/main/java/com/green/secondproject/teacher/model/SelAcaResultVo.java
@@ -0,0 +1,13 @@
+package com.green.secondproject.teacher.model;
+
+import lombok.Data;
+
+@Data
+public class SelAcaResultVo {
+    private int year;
+    private int mf;
+    private int score;
+    private int rating;
+    private int cr;
+    private int wr;
+}
