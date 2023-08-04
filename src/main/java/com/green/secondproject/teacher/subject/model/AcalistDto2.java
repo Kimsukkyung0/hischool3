d50c386628883f7b93560f7616c4cdea6cb8a6fa
성수천 <3598ysm@gmail.com>
2023-08-04 19:51:30 +0900

    insert문 로그인 facade

---
+++ src/main/java/com/green/secondproject/teacher/subject/model/AcalistDto2.java
@@ -0,0 +1,10 @@
+package com.green.secondproject.teacher.subject.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class AcalistDto2 {
+    private List<AcaSubjectVo2> list;
+}
