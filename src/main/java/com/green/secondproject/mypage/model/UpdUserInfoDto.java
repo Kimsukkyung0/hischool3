b83ae733aed2776b78b7d2b8223f28ccda782015
노정민 <ssolen@naver.com>
2023-08-09 17:09:32 +0900

    회원 정보 수정 합치기(선생,학생)

--- src/main/java/com/green/secondproject/mypage/model/UpdUserInfoDto.java
+++ src/main/java/com/green/secondproject/mypage/model/UpdUserInfoDto.java
@@ -3,8 +3,7 @@ package com.green.secondproject.mypage.model;
 import lombok.Data;
 
 @Data
-public class UpdTeacherInfoDto2 {
-    private Long userId;
+public class UpdUserInfoDto {
     private String nm;
     private String phone;
     private String address;
