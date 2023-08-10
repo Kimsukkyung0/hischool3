c9858d8e405fc58a89110700e56bc62a7ff195f4
남규진 <dft2007@naver.com>
2023-08-10 15:08:10 +0900

    통합 테스트 중

--- src/test/java/com/green/secondproject/IntegrationTest.java
+++ src/test/java/com/green/secondproject/IntegrationTest.java
@@ -1,6 +1,7 @@
 package com.green.secondproject;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
