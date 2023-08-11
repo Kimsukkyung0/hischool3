eb759b6dab070ea90c91d430fca92a8eb2abd2f1
남규진 <dft2007@naver.com>
2023-08-11 15:51:46 +0900

    jdbc-url url로 수정

--- src/main/java/com/green/secondproject/config/DatabaseConfiguration.java
+++ src/main/java/com/green/secondproject/config/DatabaseConfiguration.java
@@ -1,26 +1,26 @@
-package com.green.secondproject.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
-
-@Configuration
-@EnableTransactionManagement
-public class DatabaseConfiguration {
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
-    }
-
-    @Bean
-    public DataSourceTransactionManager transactionManager(){
-        DataSourceTransactionManager manager = new DataSourceTransactionManager(dataSource());
-        return manager;
-    }
-}
+//package com.green.secondproject.config;
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//@EnableTransactionManagement
+//public class DatabaseConfiguration {
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource dataSource() {
+//        return DataSourceBuilder.create().build();
+//    }
+//
+//    @Bean
+//    public DataSourceTransactionManager transactionManager(){
+//        DataSourceTransactionManager manager = new DataSourceTransactionManager(dataSource());
+//        return manager;
+//    }
+//}
