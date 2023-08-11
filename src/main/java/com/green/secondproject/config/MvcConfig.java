20cb3205c998fcd27c3e0bc4b4c0c069f28921ba
석민경 <smk93021@gmail.com>
2023-08-11 11:31:43 +0900

    jdbc 설정 및 mvc 설정(이미지)

---
+++ src/main/java/com/green/secondproject/config/MvcConfig.java
@@ -0,0 +1,31 @@
+package com.green.secondproject.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Slf4j
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Value("/home/download")
+    private String fileDir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        exposeDirectory(fileDir, registry);
+    }
+
+    private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
+        Path uploadDir = Paths.get(dirName);
+        String uploadPath = uploadDir.toFile().getAbsolutePath();
+        log.info("uploadPath {}", uploadPath);
+        if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
+        registry.addResourceHandler("/img/**").addResourceLocations("file:"+ uploadPath + "/");
+    }
+}
