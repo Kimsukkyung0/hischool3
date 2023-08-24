d861897cd5811d7c6039b3dd66384124e74a2b6b
성수천 <3598ysm@gmail.com>
2023-08-24 14:40:21 +0900

    jpa 공지사항 List

--- src/main/java/com/green/secondproject/notice/model/NoticeInsDto.java
+++ src/main/java/com/green/secondproject/notice/model/NoticeInsDto.java
@@ -2,11 +2,13 @@ package com.green.secondproject.notice.model;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class NoticeInsDto {
     private String title;
     private String content;
     private int hits;
     private int imptyn;
-
+    private LocalDateTime createdAt;
 }
