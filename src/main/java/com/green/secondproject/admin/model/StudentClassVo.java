213aa2f78c027b1fe2da1400709ea7b1532e489f
정민 <ssolen@naver.com>
2023-08-29 19:58:21 +0900

    user list 값 추가

--- src/main/java/com/green/secondproject/admin/model/StudentClassVo.java
+++ src/main/java/com/green/secondproject/admin/model/StudentClassVo.java
@@ -1,5 +1,6 @@
 package com.green.secondproject.admin.model;
 
+import com.green.secondproject.common.config.etc.EnrollState;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,6 +9,9 @@ import lombok.Getter;
 public class StudentClassVo {
     private long userId;
     private String nm;
+    private String email;
+    private String phone;
+    private EnrollState enrollState;
     private String grade;
     private String classNum;
 }
