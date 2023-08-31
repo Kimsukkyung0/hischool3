1944604c60c030f1866829c9b0da06b3349338c8
석민경 <smk93021@gmail.com>
2023-08-31 15:38:09 +0900

    과목관리->선생님페이지로 이동

--- src/main/java/com/green/secondproject/teacher/subject/SubjectMapper.java
+++ src/main/java/com/green/secondproject/teacher/subject/SubjectMapper.java
@@ -17,22 +17,16 @@ public interface SubjectMapper {
 
     int instcsbj(List<SubjectInsVo> list);
 
-
-
     List<SubjectVo2> smalllist(SubjectDto dto);
 
     int classnum(StudentClassDto dto);
 
     int schoolnum(StudentSchoolDto dto);
 
-
-
     //모의고사 시작
     List<MockSubjectBigVo> mockbiglist();
 
     List<MockSubjcetSmallVo> mocksmalllist(Long categoryid);
-
-
 
     List<StudentListVo> stulist(StudentListDto dto);
 
