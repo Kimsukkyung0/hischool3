b5e4b6611f95d5ee4954854bf68f4012ff626da4
남규진 <dft2007@naver.com>
2023-09-04 16:45:11 +0900

    스웨거 설명 수정

--- src/main/java/com/green/secondproject/admin/model/AdminParam.java
+++ src/main/java/com/green/secondproject/admin/model/AdminParam.java
@@ -1,11 +1,16 @@
 package com.green.secondproject.admin.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class AdminParam {
+    @Schema(example = "admin1@naver.com", description = "이메일")
     private String email;
+    @Schema(example = "gkrtod123!", description = "비밀번호")
     private String pw;
+    @Schema(example = "7240103", description = "학교코드")
     private String schoolCode;
+    @Schema(example = "청구고등학교", description = "학교명")
     private String schoolNm;
 }
--- src/main/java/com/green/secondproject/sign/model/SchoolParam.java
+++ src/main/java/com/green/secondproject/sign/model/SchoolParam.java
@@ -1,9 +1,13 @@
 package com.green.secondproject.sign.model;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class SchoolParam {
+    @Parameter(description = "학교코드", example = "7240103", required = true)
     private String schoolCode;
+    @Parameter(description = "학년", example = "1", required = true)
     private String grade;
 }
