a40a08ffbfc1f6d159978122697e93188fc4c5a8
석민경 <smk93021@gmail.com>
2023-09-06 15:57:12 +0900

    수상 entity/repository + 수상기록입력 메서드추가

---
+++ src/main/java/com/green/secondproject/awards/AwardsService.java
@@ -0,0 +1,53 @@
+package com.green.secondproject.awards;
+
+import com.green.secondproject.awards.model.AwardsInsDto;
+import com.green.secondproject.awards.model.AwardsVo;
+import com.green.secondproject.common.config.security.AuthenticationFacade;
+import com.green.secondproject.common.config.security.model.RoleType;
+import com.green.secondproject.common.entity.AwardsEntity;
+import com.green.secondproject.common.entity.UserEntity;
+import com.green.secondproject.common.repository.AwardsRepository;
+import com.green.secondproject.common.repository.UserRepository;
+import com.green.secondproject.volunteerwork.model.VolWorkInsDto;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class AwardsService {
+    private final UserRepository usrRep;
+    private final AuthenticationFacade facade;
+    private final AwardsRepository awsRep;
+
+    public AwardsVo postAward(AwardsInsDto dto){
+        UserEntity usrEnti = usrRep.findByUserId(dto.getUserId());
+        if(facade.getLoginUser().getSchoolId() == usrEnti.getVanEntity().getSchoolEntity().getSchoolId()
+                && usrEnti.getRoleType().equals(RoleType.STD)) {
+
+               AwardsEntity awsEnti =  awsRep.save(AwardsEntity.builder().
+                        userEntity(usrEnti)
+                        .nm(dto.getNm())
+                        .prize(dto.getPrize())
+                        .awardedAt(dto.getAwardedAt())
+                        .awardedBy(dto.getAwardedBy())
+                        .participation(dto.getParticipation())
+                        .build());
+
+               return AwardsVo.builder()
+                       .awardId(awsEnti.getAwardId())
+                       .userId(dto.getUserId())
+                       .nm(awsEnti.getNm())
+                       .prize(awsEnti.getPrize())
+                       .awardedAt(awsEnti.getAwardedAt())
+                       .awardedBy(awsEnti.getAwardedBy())
+                       .participation(awsEnti.getParticipation())
+                       .build();
+        }
+        else{
+            throw new RuntimeException("올바른 요청값이 아닙니다");
+        }
+    }
+}
+
